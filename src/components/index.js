11bfd561dfb1f41fb70c0617386e77a2e6d92e22
zhenyuWang <13641039885@163.com>
2023-12-22 10:31:59 +0800

    refactor: remove useless module

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
+// import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
@@ -15,10 +15,10 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
+  // Feedbacks,
   Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
 }
